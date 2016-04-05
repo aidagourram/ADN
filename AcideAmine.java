884340f6c42daa009dcfb2bcfd08a30c922cc121
aidagourram <aidagourram@gmail.com>
2016-04-05 23:07:45 +0000

    Added files via upload

---
+++ AcideAmine.java
@@ -0,0 +1,36 @@
+package dna.com;
+
+public class AcideAmine {
+	
+	public AcideAmine() {
+		String []ala = {"GCA","GCC","GCG","GCT"};
+		String []arg = {"CGA","CGC","CGG","CGT","AGA","AGG"};
+		String []asp ={"GAC","GAT"};
+		String []asn ={"AAC","AAT"};
+		String []cys ={"TGC","TGT"};
+		String []glu ={"GAA","GAG"};
+		String []gln ={"CAA","CAG"};
+		String []gly ={"GGA","GGC","GGG","GGT"};
+		String []his ={"CAC","CAT"};
+		String []ile ={"ATA","ATC","ATT"};
+		String []leu ={"CTA","CTC","CTG","CTT","TTA","TTG"};
+		String []lys ={"AAA","AAG"};
+		String []met ={"ATG"};
+		String []phe ={"TTC","TTT"};
+		String []pro ={"CCA","CCC","CCG","CCT"};
+		String []ser ={"TCA","TCC","TCG","TCT","AGC","AGT"};
+		String []thr ={"ACT","ACC","ACG","ACT"};
+		String []trp ={"TGG"};
+		String []tyr ={"TAC","TAT"};
+		String []val ={"GTA","GTC","GTG","GTT"};
+		String []stop ={"TAG","TAA","TGA"};
+		
+	}
+
+	@Override
+	public String toString() {
+		return "AcideAmine [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+}
+
